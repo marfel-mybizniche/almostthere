3b051c31acf575f34a34c9c40309368e8c03eb37
marmbn <marfel@mybizniche.com>
2020-10-30 11:23:01 +0800

    add 404 page

--- 404.php
+++ 404.php
@@ -1,19 +1,11 @@
 <?php get_header() ?>
 
-<section id="sec-404" class="sec-404 section">
+<div class="page-not-found">
     <div class="grid-container">
-        <div class="grid-x grid-margin-x">
-            <div class="cell large-12">
-                <div class="text-block">
-                    <h1>404 ERROR PAGE</h1>
-
-                    <p>Sorry, the page you are looking for doesn’t exist or an other error occured.</p>
-                    <p><a href="javascript:window.history.back()">Go back</a> or head over to <a href="<?php echo home_url() ?>"><?php echo home_url() ?></a> to choose a new direction.
-                    </p>
-                </div>
-            </div>
-        </div>
+        <h1>Page Not Found.</h1>
+        <p>The page you requested cannot be found.</p>
+        <a href="<?php echo get_home_url(); ?>" class="button primary">go back to homepage</a>
     </div>
-</section>
+</div>
 
 <?php get_footer() ?>
