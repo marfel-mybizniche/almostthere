cf6bffb2c49408e031d7cb9962fec39fdcb695a9
marmbn <marfel@mybizniche.com>
2020-10-30 21:07:39 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,10 +1,16 @@
 <?php get_header() ?>
 
-<div class="page-not-found">
+
+<div class="sec_banner">
     <div class="grid-container">
         <h1>Page Not Found.</h1>
+    </div>
+</div>
+
+<div class="page-not-found">
+    <div class="grid-container">
         <p>The page you requested cannot be found.</p>
-        <a href="<?php echo get_home_url(); ?>" class="button primary">go back to homepage</a>
+        <a href="<?php echo get_home_url(); ?>" class="button secondary">go back to homepage</a>
     </div>
 </div>
 
